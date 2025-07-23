34964924957fe60b627272ce6ab97fe8b46593cd
Marco Filho <m.barceloslima@gmail.com>
2025-07-23 00:08:51 -0700

    Enhance product and supplier views for better usability
    - Updated titles and layouts in Delete, Details, and Edit views for products and suppliers to improve clarity and user experience.
    - Introduced new partial views for product listing and address details to enhance code organization and reusability.
    - Added RazorExtensions for document formatting and simplified the structure of address-related views.
    - Included summary components and improved button styling in Index views for both products and suppliers.

---
+++ src/DevIO.App/Dev.IO.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace DevIO.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string DocumentFormatter(this RazorPage page, int type, string document)
+        {
+            return type == 1
+                ? Convert.ToUInt64(document).ToString(@"000\.000\.000\-00")
+                : Convert.ToUInt64(document).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
--- src/DevIO.App/Dev.IO.App/ViewModels/SupplierViewModel.cs
+++ src/DevIO.App/Dev.IO.App/ViewModels/SupplierViewModel.cs
@@ -1,5 +1,4 @@
-﻿using DevIO.Business.Models;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DevIO.App.ViewModels
 {
