92702c03531494be5c2987cfdf308a3483d6b126
Marco Filho <m.barceloslima@gmail.com>
2025-07-23 01:04:25 -0700

    Fix DevIO.App folder sln

---
+++ src/Dev.IO.App/Controllers/SuppliersController.cs
@@ -0,0 +1,155 @@
+﻿using AutoMapper;
+using DevIO.App.ViewModels;
+using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.App.Controllers
+{
+    public class SuppliersController : BaseController
+    {
+        private readonly ISupplierRepository _supplierRepository;
+        private readonly IAddressRepository _addressRepository;
+        private readonly IMapper _mapper;
+
+        public SuppliersController(ISupplierRepository supplierRepository, IAddressRepository addressRepository, IMapper mapper)
+        {
+            _supplierRepository = supplierRepository;
+            _addressRepository = addressRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(_mapper.Map<IEnumerable<SupplierViewModel>>(await _supplierRepository.GetAllAsync()));
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+
+            var supplierViewModel = await GetSupplierAddressAsync(id);
+
+            if (supplierViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplierViewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SupplierViewModel supplierViewModel)
+        {
+            if (!ModelState.IsValid) return View(supplierViewModel);
+
+            var supplier = _mapper.Map<Supplier>(supplierViewModel);
+
+            await _supplierRepository.AddAsync(supplier);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var supplierViewModel = await GetSupplierProductsAddressAsync(id);
+
+            if (supplierViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplierViewModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, SupplierViewModel supplierViewModel)
+        {
+            if (id != supplierViewModel.Id) return NotFound();
+
+            if (!ModelState.IsValid) return View(supplierViewModel);
+
+            var supplier = _mapper.Map<Supplier>(supplierViewModel);
+
+            await _supplierRepository.UpdateAsync(supplier);
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var supplierViewModel = await GetSupplierAddressAsync(id);
+
+            if (supplierViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplierViewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var supplierViewModel = await GetSupplierAddressAsync(id);
+
+            if (supplierViewModel == null) return NotFound();
+
+            await _supplierRepository.RemoveAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> GetAddress(Guid id)
+        {
+            var supplier = await GetSupplierAddressAsync(id);
+            if (supplier == null) return NotFound();
+
+            return PartialView("_AddressDetails", supplier);
+        }
+
+        public async Task<IActionResult> UpdateAddress(Guid id)
+        {
+            var supplier = await GetSupplierAddressAsync(id);
+            if (supplier == null) return NotFound();
+
+            return PartialView("_UpdateAddress", new SupplierViewModel { Address = supplier.Address });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateAddress(SupplierViewModel supplierViewModel)
+        {
+            if (!ModelState.IsValid) return PartialView("_UpdateAddress", supplierViewModel);
+
+            await _supplierRepository.UpdateAsync(_mapper.Map<Supplier>(supplierViewModel));
+
+            var url = Url.Action("GetAddress", "Suppliers", new { id = supplierViewModel.Address.SupplierId });
+            return Json(new { success = true, url });
+        }
+
+        private async Task<SupplierViewModel> GetSupplierAddressAsync(Guid id)
+        {
+            return _mapper.Map<SupplierViewModel>(await _supplierRepository.GetSupplierAddress(id));
+        }
+
+        private async Task<SupplierViewModel> GetSupplierProductsAddressAsync(Guid id)
+        {
+            return _mapper.Map<SupplierViewModel>(await _supplierRepository.GetSupplierProductsAddress(id));
+        }
+
+        private bool IsValidOperation()
+        {
+            return ModelState.IsValid;
+        }
+    }
+}
