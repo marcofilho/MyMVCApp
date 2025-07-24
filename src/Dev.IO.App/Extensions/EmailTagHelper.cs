3c0bcbc0b41a987e7ef37e897bfa5ac503707848
Marco Filho <m.barceloslima@gmail.com>
2025-07-24 02:39:19 -0700

    Update email handling and footer content
    Modified `EmailTagHelper.cs` to change the default email
    domain to "gmail.com" and set a specific email name
    "m.barceloslima". Updated email address construction logic
    to use the new `EmailName` property.
    
    Revised `_Layout.cshtml` footer to remove previous email
    tags and include a link to a GitHub profile, displaying
    the name "Marco Lima" as an email tag for improved
    content and structure.

--- src/Dev.IO.App/Extensions/EmailTagHelper.cs
+++ src/Dev.IO.App/Extensions/EmailTagHelper.cs
@@ -4,12 +4,12 @@ namespace DevIO.App.Extensions
 {
     public class EmailTagHelper : TagHelper
     {
-        public string EmailDomain { get; set; } = "github.com/marcofilho";
+        public string EmailDomain { get; set; } = "gmail.com";
+        public string EmailName { get; set; } = "m.barceloslima";
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
-            var content = await output.GetChildContentAsync();
-            var target = content.GetContent() + "@" + EmailDomain;
+            var target = EmailName+ "@" + EmailDomain;
             output.Attributes.SetAttribute("href", "mailto:" + target);
             output.Content.SetContent(target);
         }
